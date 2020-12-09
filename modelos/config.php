7ae49243ed78add6bb3321419ee79855caf32821
erredege <34535136+erredege@users.noreply.github.com>
2020-12-09 19:44:59 +0100

    modifico config 9/12/20 19:44

--- modelos/config.php
+++ modelos/config.php
@@ -4,3 +4,10 @@ define( 'DB_NAME', 'reservas' );
 define( 'DB_USER', 'root' );
 define( 'DB_PASSWORD', '');
 define( 'DB_HOST', 'localhost' );
+
+/*
+define( 'DB_NAME', 'reservas-rosendo' );
+define( 'DB_USER', 'rosen' );
+define( 'DB_PASSWORD', 'Rosen123*');
+define( 'DB_HOST', 'localhost' );
+*/
